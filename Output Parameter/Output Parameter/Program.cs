7ad6641d79157e176e50f4b2c87fc869b966f062
Raufull Islam Rauf <152892595+Rauf-17@users.noreply.github.com>
2024-04-05 08:59:53 +0600

    Add files via upload
    The out parameter in C# is used to pass arguments to methods by reference.

---
+++ Output Parameter/Output Parameter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output_Parameter
+{
+    public class Program
+    {
+        public static void calculate(int x, int y, out int add, out int sub, out int mul, out int div)
+        {
+            add = x + y;
+            sub = x - y;
+            mul = x * y;
+            div = x / y;
+        }
+        static void Main(string[] args)
+        {
+            int num1, num2, a, b, c, d;
+
+            Console.WriteLine("Enter Any Values :");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            calculate(num1, num2,out a,out b,out c,out d);
+
+            Console.WriteLine("Addition = " +a);
+            Console.WriteLine("Substration = " +b);
+            Console.WriteLine("Multiplication = " +c);
+            Console.WriteLine("Division = " +d);
+
+            Console.ReadKey();
+        }
+    }
+}
