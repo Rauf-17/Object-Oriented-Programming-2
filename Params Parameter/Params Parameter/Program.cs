840a8c273c9a738b0c9de6cd7b28592e0d2c0c7c
Raufull Islam Rauf <152892595+Rauf-17@users.noreply.github.com>
2024-04-05 09:01:17 +0600

    Add files via upload
    The "params" keyword in C# allows a method to accept a variable number of arguments. C# params works as an array of objects. By using params keyword in a method argument definition, we can pass a number of arguments. Note: There can't be anymore parameters after a params.

---
+++ Params Parameter/Params Parameter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params_Parameter
+{
+    internal class Program
+    {
+        public static void print(params int[]numbers)
+        {
+            Console.WriteLine("\nThere is {0} element :", numbers.Length);
+            for(int i = 0;i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]+ " ");
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            print();
+            print(10, 20);
+            print(20, 30,40);
+
+            Console.ReadKey();
+        }
+    }
+}
