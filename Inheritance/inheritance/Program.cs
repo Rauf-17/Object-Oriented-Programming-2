d69035b153ed1f1fb50de4d3316a9deb0324bbc3
Raufull Islam Rauf <152892595+Rauf-17@users.noreply.github.com>
2024-05-27 01:08:37 +0600

    Add files via upload
    Ihere the code for Inheritence in C#

---
+++ Inheritance/inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    public class Animals
+    {
+        public string color;
+    }
+    public class Dog : Animals
+    {
+        public string type;
+
+
+        public void showDetails()
+        {
+            Console.WriteLine("Color of The Animal: " + color);
+            Console.WriteLine("Type of The Animal: " +  type);
+        }
+
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            Dog dog = new Dog();
+            dog.color = "Red";
+            dog.type = "German Shepard";
+            dog.showDetails();
+
+            Console.ReadKey();
+        }
+    }
+}
