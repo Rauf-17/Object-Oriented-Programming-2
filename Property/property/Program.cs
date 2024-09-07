c2ba26caef86f978e7ee7036836d49b7d1c0d4fd
Raufull Islam Rauf <152892595+Rauf-17@users.noreply.github.com>
2024-09-07 15:24:09 +0600

    Add files via upload

---
+++ Property/property/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace property
+{
+    class student
+    {
+        private string id = "21-45777-3";
+        public string ID
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new student();
+            s.ID = ("21-45779-3");
+            Console.WriteLine(s.ID);
+            Console.ReadKey();
+
+        }
+    }
+}
